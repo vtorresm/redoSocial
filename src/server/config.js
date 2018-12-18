c8a7c3d6c9a8b17afd4dc02f4c143d74f87093a4
Victor Torres <vatorresm88@gmail.com>
2018-12-17 22:19:24 -0500

    Configurando proyecto

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,36 @@
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+const morgan = require('morgan');
+const multer = require('multer');
+const express = require('express');
+
+const routes = require('../routes/index');
+
 module.exports = app => {
 
     //Settings
     app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('.hbs', exphbs({
+       defaultLayout: 'main',
+       partialsDir: path.join(app.get('views'), 'partials'),
+       layoutsDir: path.join(app.get('views'), 'layouts'),
+       extname: '.hbs',
+       helpers: require('./helpers')
+    }));
+    app.set('view engine', '.hbs');
+
+    //middlewares
+    app.use(morgan('dev'));
+    app.use(multer({dest: path.join(__dirname, '../public/upload/temp')}).single('image'));
+    app.use(express.urlencoded({extended: false}));
+    app.use(express.json());
+
+    //routes
+    routes(app);
+
+    //errorhandlers
     
     return app;
 }
