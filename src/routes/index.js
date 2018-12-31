6545f777475d5d08d7ee481217b5e3759a8e92d2
Victor Torres <vatorresm88@gmail.com>
2018-12-30 23:10:03 -0500

    Creación y manejo  de rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controllers/home');
+const image = require('../controllers/image');
+
 module.exports = app => {
 
-    app.get('/', (req, res) => {
-        res.send('Index page');
-    });
+  router.get('/', home.index);
+  router.get('/images/:image_id', image.index);
+  router.post('/images', image.create);
+  router.post('/images/:image_id/like', image.like);
+  router.post('/images/:image_id/comment', image.comment);
+  router.post('/images/:image_id', image.remove);
+
+
+  app.use(router);
 
 };
