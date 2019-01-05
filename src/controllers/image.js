218a2a6fb7d01d05606c9266678585b23a844b1b
Victor Torres <vatorresm88@gmail.com>
2019-01-05 13:31:49 -0500

    Guardando imagenes en Mongodb

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,36 +2,56 @@ const path = require('path');
 const { randomNumber } = require('../helpers/libs');
 const fs = require('fs-extra');
 
+const { Image } = require('../models');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = async (req, res) => {
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+ctrl.create = (req, res) => {
 
-    if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.git') {
-        await fs.rename(imageTempPath, targetPath);
-    }
+    const saveImage = async () => {
+        const imgUrl = randomNumber();
+        const images = await Image.find({ filename: imgUrl });
+        if (images.length > 0) {
+            saveImage();
+        } else {
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
 
-    res.send('works!');
+            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.git') {
+                await fs.rename(imageTempPath, targetPath);
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imgUrl + ext,
+                    description: req.body.description
+                });
+                const imageSaved = await newImg.save();
+                //res.redirect('/images/:image_id');
+                res.send('works');
+            } else {
+                await fs.unlink(imageTempPath);
+                res.status(500).json({ error: 'Only Image are allowed' });
+            }
+        } 
+    };
+    saveImage();
 };
 
 ctrl.like = (req, res) => {
-    
+
 };
 
 ctrl.comment = (req, res) => {
-    
+
 };
 
 ctrl.remove = (req, res) => {
-    
+
 };
 
 module.exports = ctrl;
